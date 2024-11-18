d845df83fad7d00eb8f84a8a47badac5a7ee080a
Arack <arack.rs@gmail.com>
2024-11-18 17:51:18 -0500

    feat(screen): add initial, login, register and main screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+import 'package:bioface_flutter/screens/register_screen.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:bioface_flutter/screens/main_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../model/login_request.dart';
+import '../model/login_response.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    final url = Uri.parse('https://groweasy.azurewebsites.net/api/v1/auth/log-in');
+    final loginRequest = LoginRequest(
+      username: _usernameController.text,
+      password: _passwordController.text,
+    );
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(loginRequest.toJson()),
+      );
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final loginResponse = LoginResponse.fromJson(jsonResponse);
+
+        if (loginResponse.status) {
+          // Guardar el JWT y el nombre de usuario localmente usando SharedPreferences
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setString('jwt', loginResponse.jwt);
+          await prefs.setString('username', _usernameController.text);
+
+          // Navegar a MainScreen si el login es exitoso
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return const MainScreen();
+              },
+            ),
+          );
+        } else {
+          _showError(loginResponse.message);
+        }
+      } else {
+        final jsonResponse = json.decode(response.body);
+        final errorMessage = jsonResponse['message'] ?? 'Error: ${response.statusCode}';
+        _showError(errorMessage);
+      }
+    } catch (error) {
+      _showError('Error: No se pudo conectar al servidor');
+    }
+  }
+
+  void _showError(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Icon(
+                  Icons.account_circle_outlined,
+                  size: 90,
+                ),
+                const Text(
+                  'Iniciar Sesión',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Divider(
+                    endIndent: 20, indent: 20, height: 60, thickness: 3),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombre de usuario',
+                    hintText: 'Ingrese su nombre de usuario',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Contraseña',
+                    hintText: 'Ingrese su contraseña',
+                  ),
+                ),
+                const Divider(
+                    endIndent: 20, indent: 20, height: 60, thickness: 3),
+                RichText(
+                  text: TextSpan(
+                    text: '¿Aún no tienes una cuenta? ',
+                    style: const TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Regístrate aquí!',
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return RegisterScreen();
+                                },
+                              ),
+                            );
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                FilledButton(
+                  onPressed: _login,
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 15),
+                    textStyle: const TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  child: const Text('Iniciar Sesión'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,201 @@
+import 'dart:convert';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'login_screen.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+
+  Future<void> _signup() async {
+    const String signupEndpoint = 'https://groweasy.azurewebsites.net/api/v1/auth/sign-up';
+
+    // Verificar si las contraseñas coinciden
+    if (_passwordController.text != _confirmPasswordController.text) {
+      _showError('Las contraseñas no coinciden');
+      return;
+    }
+
+    // Construir la solicitud
+    final Map<String, dynamic> signupData = {
+      'firstName': _firstNameController.text,
+      'lastName': _lastNameController.text,
+      'username': _usernameController.text,
+      'password': _passwordController.text,
+      'role': 'ADMIN', // Ajustar según sea necesario
+    };
+
+    try {
+      // Realizar la solicitud POST
+      final response = await http.post(
+        Uri.parse(signupEndpoint),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(signupData),
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        // Registro exitoso, mostrar mensaje y redirigir al login
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Registro exitoso. Por favor, inicia sesión.')),
+        );
+
+        // Redirigir a la pantalla de inicio de sesión
+        Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+              (Route<dynamic> route) => false,
+        );
+      } else {
+        // Manejar errores
+        final jsonResponse = json.decode(response.body);
+        final errorMessage = jsonResponse['message'] ?? 'Error: ${response.statusCode}';
+        _showError(errorMessage);
+      }
+    } catch (error) {
+      _showError('Error al conectar con el servidor: $error');
+    }
+  }
+
+  void _showError(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Crear Cuenta',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Divider(
+                  endIndent: 20,
+                  indent: 20,
+                  height: 60,
+                  thickness: 3,
+                ),
+                TextField(
+                  controller: _firstNameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombre',
+                    hintText: 'Ingrese su nombre',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _lastNameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Apellido',
+                    hintText: 'Ingrese su apellido',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombre de usuario',
+                    hintText: 'Ingrese su nombre de usuario',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Contraseña',
+                    hintText: 'Ingrese su contraseña',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirmar Contraseña',
+                    hintText: 'Confirme su contraseña',
+                  ),
+                ),
+                const Divider(
+                  endIndent: 20,
+                  indent: 20,
+                  height: 60,
+                  thickness: 3,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: '¿Ya tienes una cuenta? ',
+                    style: const TextStyle(
+                      fontSize: 13,
+                      color: Colors.black,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Inicia sesión aquí',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginScreen()),
+                                  (Route<dynamic> route) => false,
+                            );
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FilledButton(
+                      onPressed: _signup,
+                      child: const Text('Registrarse'),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancelar'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
